73c8bebb9bf943e3283ad1b1d3c75877910325d1
Carson <crkruege@gmail.com>
2023-11-10 09:31:59 -0700

    added derive store for board state & defaults

--- src/library/board.rs
+++ src/library/board.rs
@@ -1,9 +1,11 @@
 use crate::library::piece::{MoveError, Piece};
 use crate::library::player_state::{PlayerColor, PlayerState};
 use crate::library::point::Point;
+use yewdux::prelude::*;
 
 static MAX_BOARD_WIDTH: u8 = 8;
 
+#[derive(Store, PartialEq)]
 pub struct BoardState {
     turn: PlayerColor,
     pub w_player: PlayerState,
@@ -19,6 +21,16 @@ impl BoardState {
             return false;
         } else {
             return true;
+        }
+    }
+}
+
+impl Default for BoardState {
+    fn default() -> Self {
+        BoardState {
+            turn: PlayerColor::WHITE,
+            w_player: PlayerState::new(PlayerColor::WHITE),
+            b_player: PlayerState::new(PlayerColor::BLACK),
         }
     }
 }
--- src/library/player_state.rs
+++ src/library/player_state.rs
@@ -2,30 +2,15 @@ use super::piece::PieceType;
 use crate::library::{piece::Piece, point::Point};
 use collect_slice::CollectSlice;
 
-#[derive(Clone, Copy)]
+#[derive(PartialEq, Clone, Copy)]
 pub enum PlayerColor {
     WHITE,
     BLACK,
 }
 
+#[derive(PartialEq)]
 pub struct PlayerState {
     pub color: PlayerColor,
-    // pub king: Option<Piece>,
-    // pub queen: Option<Piece>,
-    // pub bishop1: Option<Piece>,
-    // pub bishop2: Option<Piece>,
-    // pub rook1: Option<Piece>,
-    // pub rook2: Option<Piece>,
-    // pub knight1: Option<Piece>,
-    // pub knight2: Option<Piece>,
-    // pub pawn1: Option<Piece>,
-    // pub pawn2: Option<Piece>,
-    // pub pawn3: Option<Piece>,
-    // pub pawn4: Option<Piece>,
-    // pub pawn5: Option<Piece>,
-    // pub pawn6: Option<Piece>,
-    // pub pawn7: Option<Piece>,
-    // pub pawn8: Option<Piece>,
     pub pieces: [Option<Piece>; 16],
 }
 
@@ -35,7 +20,7 @@ pub enum PlayerStateError {
 }
 
 impl PlayerState {
-    fn new(color: PlayerColor) -> PlayerState {
+    pub fn new(color: PlayerColor) -> PlayerState {
         PlayerState {
             color: color,
             pieces: [
